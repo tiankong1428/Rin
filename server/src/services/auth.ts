55c2a6266e7b3eeff5c5713ec79586c09792ad35
tiankong1428 <3166140298@qq.com>
2026-06-21 14:47:47 +0800

    更新 auth.ts

--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -9,15 +9,8 @@ import {
     ForbiddenError,
     InternalServerError,
 } from "../errors";
+import { hashPassword } from "../utils/password";
 
-// Hash password using SHA-256
-async function hashPassword(password: string): Promise<string> {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(password);
-    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    return hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
-}
 
 export function PasswordAuthService(): Hono<{
         Bindings: Env;
