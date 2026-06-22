af5a1450c6041cad716728a16fbf798d22f11fe6
tiankong1428 <3166140298@qq.com>
2026-06-22 20:00:18 +0800

    更新 tag.ts

--- server/src/services/tag.ts
+++ server/src/services/tag.ts
@@ -40,7 +40,7 @@ export function TagService(): Hono {
                                     );
                                 }
                             }
-                        }
+                        } as any
                     }
                 }
             }
