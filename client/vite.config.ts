e4446c37c97c16059b9a1435589abbcbb7488814
tiankong1428 <3166140298@qq.com>
2026-07-04 02:34:45 +0800

    更新 vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
   const cacheDir = process.env.RIN_VITE_CACHE_DIR || "../.vite/client";
   
   return {
-    base: "./",
+    base: "/",
     cacheDir,
     build: {
       outDir: '../dist/client',
